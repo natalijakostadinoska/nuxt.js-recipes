c6e63105d4a89abb085a73f44a31da0d22e83af7
natalijakostadinoska <74496725+natalijakostadinoska@users.noreply.github.com>
2021-03-17 22:06:52 +0100

    Fourth

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="single-recipe">
+    <h1>{{ recipe.title }}</h1>
+    <div>
+      <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title" />
+    </div>
+    <p>{{ recipe.descriptionText }}</p>
+  </div>
+</template>
+
+<style scoped>
+.single-recipe {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 30px;
+}
+</style>
+
+<script>
+export default {
+    asyncData (context) {
+        return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                recipe: [
+                  {
+                    id: '1',
+                    title: 'Delicious Pasta',
+                    previewText: 'Awesome Pasta!',
+                    descriptionText: 'Delicious meal',
+                    thumbnail: 'https://hips.hearstapps.com/delish/assets/17/36/1504715566-delish-fettuccine-alfredo.jpg'
+                },
+                {
+                    id: '2',
+                    title: 'Delicious Avocado Toast',
+                    previewText: 'Very Tasty!!!',
+                    descriptionText: 'Delicious meal',
+                    thumbnail: 'https://eluxemagazine.com/wp-content/uploads/2017/05/avocado-recipes.jpg'
+                  }
+               ].find(el => el.id === context.params.id)
+            })
+        }, 1500)
+        })
+    }
+}
+</script>
+
+<style scoped>
+.recipes {
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 30px;
+}
+
+.recipe-image {
+  width: 100%;
+}
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="recipes">
+    <Recipe
+      v-for="recipe in recepies"
+      :key="recipe.id"
+      :thumbnail="recipe.thumbnail"
+      :title="recipe.title"
+      :id="recipe.id"
+      :previewText="recipe.previewText"
+    />
+  </section>
+</template>
+
+<script>
+import Recipe from '@/components/Recipe'
+export default {
+    components: {
+        Recipe
+    },
+    asyncData () {
+        return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        resolve({
+            recepies: [
+                {
+                    id: '1',
+                    title: 'Delicious Pasta',
+                    previewText: 'Awesome Pasta!',
+                    thumbnail: 'https://hips.hearstapps.com/delish/assets/17/36/1504715566-delish-fettuccine-alfredo.jpg'
+                },
+                {
+                    id: '2',
+                    title: 'Delicious Avocado Toast',
+                    previewText: 'Very Tasty!!!',
+                    thumbnail: 'https://eluxemagazine.com/wp-content/uploads/2017/05/avocado-recipes.jpg'
+                  }
+               ]
+             })
+         }, 1500)
+       })
+      }
+};
+</script>
+
+<style scoped>
+.recipes {
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  align-items: center;
+}
+</style>
